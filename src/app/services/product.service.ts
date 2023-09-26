e0f497ed3d6d4316164a2d120bd2f55c7458cab8
Gab Silv <gabsilv219@gmail.com>
2023-09-26 01:17:05 -0400

    new api added

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,30 +9,28 @@ import { Product } from '../interfaces/product';
 })
 export class ProductService {
   private myAppUrl: string;
-  private myApiUrl: string;
 
   constructor(private http: HttpClient) {
     this.myAppUrl = environment.endpoint;
-    this.myApiUrl = 'api/products'
   }
 
   getListProducts(): Observable<Product[]> {
-   return this.http.get<Product[]>(`${this.myAppUrl}${this.myApiUrl}`);
+   return this.http.get<Product[]>(`${this.myAppUrl}/products`);
   }
 
   deleteProduct(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.myAppUrl}${this.myApiUrl}/${id}`)
+    return this.http.delete<void>(`${this.myAppUrl}/delete-product/${id}`)
   }
 
   saveProduct(product: Product): Observable<void> {
-    return this.http.post<void>(`${this.myAppUrl}${this.myApiUrl}`,product)
+    return this.http.post<void>(`${this.myAppUrl}/create-product`,product)
   }
 
   getProduct(id: number): Observable<Product> {
-    return this.http.get<Product>(`${this.myAppUrl}${this.myApiUrl}/${id}`)
+    return this.http.get<Product>(`${this.myAppUrl}/product/${id}`)
   }
 
   updateProduct(id: number, product: Product): Observable<void> {
-    return this.http.put<void>(`${this.myAppUrl}${this.myApiUrl}/${id}`, product);
+    return this.http.put<void>(`${this.myAppUrl}/update-product/${id}`, product);
   }
 }
